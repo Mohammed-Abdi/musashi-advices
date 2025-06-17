6c0e514dbe8f29ca22a6b8b530f244b31671dab0
Mohammed <mohammedabdi.ta@gmail.com>
2025-06-17 10:56:47 +0300

    feat: implement advice retrieval and display functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import "./styles/App.css";
-
+import advices from "./data/advices.js";
+import { useState } from "react";
 function App() {
+  let randomNumber = Math.floor(Math.random() * advices.length);
+  const [id, setID] = useState(randomNumber);
+  const [count, setCount] = useState(0);
+
+  function countAdvices() {
+    setCount((c) => c + 1);
+  }
+
+  function getAdvice() {
+    setID(randomNumber);
+    countAdvices();
+  }
+
   return (
     <div className="wrapper">
       <img
@@ -10,8 +24,16 @@ function App() {
         height={150}
       />
       <div className="advice-card">
-        <div className="display"></div>
-        <button>Get Advice</button>
+        <div className="display">
+          {advices.find((advice) => advice.id === id)?.advice}
+        </div>
+        <p>
+          {count >= 100
+            ? "You have read all the advices."
+            : `You have read ${count} advice${count === 1 ? "" : "s"}.`}
+        </p>
+
+        <button onClick={getAdvice}>Get Advice</button>
       </div>
     </div>
   );
